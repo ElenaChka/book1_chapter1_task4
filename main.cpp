b8fdb21546bc3d3841c7f5d861ce0d7febc10a27
ElenaChka <checkalina.lena@bk.ru>
2021-03-14 17:35:29 +0300

    Sashylechka

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//программа, отображающая числа Фибоначчи
+//программа, отображающая числа 💓💕Фибоначчи💕💓
 #include <iostream>
 using namespace std;
 int main ()
@@ -7,19 +7,19 @@ int main ()
     cout << "Vvedite colichestvo chisel: ";
     cin >> k;
     int a, b, c;
-    a= 1;
-    b= 1;
-    if (k<= 0)
+    a = 1;
+    b = 1;
+    if (k <= 0)
         cout << "oshibka";
-    else if (k==1)
+    else if (k == 1)
         cout << "1";
     else
     cout << a << endl << b << endl;
-    for (int i=0; i<k-2; i++)
+    for (int i = 0; i < k - 2; i++)
     {
-        c= a+b;
+        c = a + b;
         cout << c << endl;
-        a= b;
-        b= c;
+        a = b;
+        b = c;
     }
 }
